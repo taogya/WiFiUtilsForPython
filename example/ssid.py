d2eb420ba657bd1cb201d5620e0968b3b11361fe
Taogya <132679709+taogya@users.noreply.github.com>
2023-06-01 16:43:45 +0900

    fix example error.

--- example/ssid.py
+++ example/ssid.py
@@ -4,9 +4,9 @@ from wifi_utils import ssid
 my_ssid = 'raspberrypi-ssid'
 my_pass = 'Raspberrypi-pass0001'
 
-print('check ssid -> ' + ssid.check_ssid(my_ssid))
-print('check pass -> ' + ssid.check_passphrase(my_pass))
-print('check pass_ex -> ' + ssid.check_passphrase_ex(my_pass))
+print('check ssid -> ' + f'{ssid.check_ssid(my_ssid)}')
+print('check pass -> ' + f'{ssid.check_passphrase(my_pass)}')
+print('check pass_ex -> ' + f'{ssid.check_passphrase_ex(my_pass)}')
 
 print('create pass -> ' + ssid.create_passphrase(13))
 print('create pass_ex -> ' + ssid.create_passphrase_ex(13))
